1957dd5ebe0ecfa8fff2a246d18e2c842695e04f
xys1223553317 <1223553317@qq.com>
2019-05-15 22:46:31 +0800

    消费者

---
+++ src/main/java/com/jk/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.jk.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/jk/consumer/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.jk.consumer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("page")
+public class PageController {
+    @RequestMapping("test")
+    @ResponseBody
+    public String  test(){
+        return "test";
+    }
+}
